33463226e83d48936e4393d52254365d56e234fd
Devil7DK <dineshthangavel47@gmail.com>
2019-02-27 15:12:16 +0530

    Add project files.

---
+++ PDF Security Remover/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PDF Security Remover")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PDF Security Remover")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("488927f0-9529-4859-b74a-c84c9ea6b022")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ PDF Security Remover/PasswordRemover.vb
@@ -0,0 +1,54 @@
+﻿Imports DevExpress.Pdf
+
+Module PasswordRemover
+
+#Region "Variables"
+    Dim Password As String
+    Dim PasswordFailed As Boolean = False
+#End Region
+
+#Region "Subs"
+    Sub Main()
+        If My.Application.CommandLineArgs.Count > 0 Then
+            For Each File As String In My.Application.CommandLineArgs
+                If My.Computer.FileSystem.FileExists(File) AndAlso IO.Path.GetExtension(File) = ".pdf" Then
+                    Console.WriteLine("Info: Processing File - {0}", IO.Path.GetFileName(File))
+                    Using Doc As New PdfDocumentProcessor
+                        AddHandler Doc.PasswordRequested, AddressOf PasswordRequested
+                        Try
+LoadDocument:
+                            Doc.LoadDocument(File)
+                            Doc.SaveDocument(File)
+                        Catch ex As PdfIncorrectPasswordException
+                            If Not PasswordFailed Then
+                                Console.WriteLine("Error: Incorrect Password..! Try Again!")
+                                PasswordFailed = True
+                                Password = ""
+                                GoTo LoadDocument
+                            Else
+                                Console.WriteLine("Error: Process Failed. Press any key to exit...")
+                                Console.Read()
+                                End
+                            End If
+                        End Try
+                    End Using
+                End If
+            Next
+        End If
+
+        Console.WriteLine("Process Completed. Press any key to exit...")
+        Console.Read()
+    End Sub
+#End Region
+
+#Region "Events"
+    Private Sub PasswordRequested(ByVal sender As Object, ByVal e As PdfPasswordRequestedEventArgs)
+        If Password = "" Then
+            Console.Write("Enter Password:")
+            Password = Console.ReadLine
+        End If
+        e.PasswordString = Password
+    End Sub
+#End Region
+
+End Module
